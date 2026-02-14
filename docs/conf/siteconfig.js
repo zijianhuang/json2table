b9910d588ce724735f8d9fd1ca86b3a9ab53c3f6
Z <zijianhuang2000@gmail.com>
2026-02-14 20:05:07 +1000

    ok

--- docs/conf/siteconfig.js
+++ docs/conf/siteconfig.js
@@ -1,9 +1,9 @@
 const SITE_CONFIG = {
 	aboutUrl: "https://jsontotable.fonlow.org/",
 	themesDic: {
-		"rose-red.css":{name: "Roes & Red", dark:false},
-		"azure-blue.css":{name: "Azure & Blue", dark:false},
-		"magenta-violet.css":{name: "Magenta & Violet", dark:true},
-		"cyan-orange.css":{name: "Cyan & Orange", dark:true}
+		"assets/themes/rose-red.css":{display: "Roes & Red", dark:false},
+		"assets/themes/azure-blue.css":{display: "Azure & Blue", dark:false},
+		"assets/themes/magenta-violet.css":{display: "Magenta & Violet", dark:true},
+		"assets/themes/cyan-orange.css":{display: "Cyan & Orange", dark:true}
 	}
 }
