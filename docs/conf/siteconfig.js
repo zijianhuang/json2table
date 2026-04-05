a37896c5385e9c9660c41c81b42746e818c74fb4
Z <zijianhuang2000@gmail.com>
2026-04-06 06:29:03 +1000

    updated

--- docs/conf/siteconfig.js
+++ docs/conf/siteconfig.js
@@ -1,9 +1,21 @@
-const SITE_CONFIG = {
+const SITE_CONFIG={
+	aboutUrl: "https://jsontotable.fonlow.org/",
+}
+
+const THEME_CONFIG = {
 	aboutUrl: "https://jsontotable.fonlow.org/",
 	themesDic: {
-		"assets/themes/rose-red.css":{display: "Roes & Red", dark:false},
-		"assets/themes/azure-blue.css":{display: "Azure & Blue", dark:false},
-		"assets/themes/magenta-violet.css":{display: "Magenta & Violet", dark:true},
-		"assets/themes/cyan-orange.css":{display: "Cyan & Orange", dark:true}
+		"assets/themes/rose-red.css": { display: "Roes & Red", dark: false },
+		"assets/themes/azure-blue.css": { display: "Azure & Blue", dark: false },
+		"assets/themes/magenta-violet.css": { display: "Magenta & Violet", dark: true },
+		"assets/themes/cyan-orange.css": { display: "Cyan & Orange", dark: true }
+	},
+	themeLoaderSettings: {
+		storageKey: 'app.theme',
+		themeLinkId: 'theme',
+		appColorsDir: 'conf/',
+		appColorsLinkId: 'app-colors',
+		colorsCss: 'colors.css',
+		colorsDarkCss: 'colors-dark.css'
 	}
 }
---
+++ docs/scripts-KZYD74EX.js
@@ -0,0 +1,1 @@
+const BUILD_META={buildTime:1775418448808};
